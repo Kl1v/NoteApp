aec049e476a2ae91cd6b73589a139040b13bf021
User <moritz.grasberger@gmx.at>
2025-03-18 11:00:13 +0100

    Delete Button Main Page

--- NoteAppBlazor/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ NoteAppBlazor/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfbe7187c80419523fadc9175efb82006d9c864a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f527d9d32fabd6670e9d2c96aac240bb20600139")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
