d88ae2bf9188cee3585b92a42c10521488df214f
Laurenz Pichler <l.pichler@htlkrems.at>
2025-03-21 16:23:51 +0100

    Register

--- NoteAppBlazor/Model/obj/Debug/net9.0/Model.AssemblyInfo.cs
+++ NoteAppBlazor/Model/obj/Debug/net9.0/Model.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Model")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eed1be00f17974a79fcb134b300f4906d69b050")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eaf54e61d1707c12d079eb262640bb6e1c622cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Model")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Model")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
