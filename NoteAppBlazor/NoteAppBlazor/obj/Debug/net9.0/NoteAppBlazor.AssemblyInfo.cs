3df8f6fdb3abcae0cf3a3df5613d4f9bf68a77d6
janTiefenbacher <160015456+janTiefenbacher@users.noreply.github.com>
2025-04-01 11:39:24 +0200

    Design

--- NoteAppBlazor/NoteAppBlazor/obj/Debug/net9.0/NoteAppBlazor.AssemblyInfo.cs
+++ NoteAppBlazor/NoteAppBlazor/obj/Debug/net9.0/NoteAppBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteAppBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8979ac8d1ca65d2189e309827ee37e7a551d5d1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b478d80d7bffa60ba3198811b53bd08b80bb31")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteAppBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteAppBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
